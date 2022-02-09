0365fce2154df00869e70d1dbabab416f8c41735
juanleguizamon <jleguizamon02@ufl.edu>
2022-02-09 08:25:06 -0600

    added placeholder div and color variables

--- js/footer.js
+++ js/footer.js
@@ -6,7 +6,33 @@ class Footer extends HTMLElement{
     connectedCallback() {
         this.innerHTML = `
         <footer>
-            <h1>Test Custom Reusable Footer</h1>
+            <div>
+                <div>
+                    <p>Social Icon</p>
+                </div>
+                <div>
+                    <p>Social Icon</p>
+                </div>
+            </div>
+            <div>
+                <ul class="nav justify-content-end">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Menu</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Catering</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Join our team</a>
+                    </li>
+                </ul>
+            </div>
         </footer>
         `;
     }
--- js/header.js
+++ js/header.js
@@ -6,7 +6,28 @@ class Header extends HTMLElement{
     connectedCallback() {
         this.innerHTML = `
         <header>
-            <h1>Test Custom Reusable Header</h1>
+            <div>
+                <p>Logo goes here</p>
+            </div>
+            <div>
+                <ul class="nav justify-content-end">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Menu</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Catering</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Join our team</a>
+                     </li>
+                </ul>
+            </div>
         </header>
         `;
     }
